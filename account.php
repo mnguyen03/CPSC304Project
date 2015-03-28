5375047c769e7c959f727f2e9d30a263ee22807d
Li Jye Tong <ljtong1@gmail.com>
2015-03-27 22:02:05 -0700

    Added in clear purchase history

--- account.php
+++ account.php
@@ -1,4 +1,224 @@
 <html>
-<a href="account.php">
-<IMG SRC="404.jpg" width="500" height="500">
+<style media="screen" type="text/css">
+
+a {
+	color: white;
+	text-decoration: none;
+}
+
+#wrapper .column {
+	position: relative;
+	float: left;
+}
+
+#left {
+	width: 25%;
+}
+
+#middle {
+	width: 50%;
+}
+
+#right {
+	width: 25%;
+}
+
+#navbar {
+	width: 25%;
+}
+
+#display {
+	width: 60%;
+	padding-top: 7%;
+	padding-left: 7%;
+}
+
+#header {
+	background: #384E82;
+	border: 1px solid white;
+}
+
+#searchbar {
+	padding-bottom: 15px;
+}
+
+#statusbar {
+	margin-bottom: 15px;
+	color: white;
+	background-color: green;
+}
+
+.search{
+	width: 350px;
+}
+
+input#logoutbtn {
+	border: 0px;
+	color: white;
+	background: #051C50;
+	font-family: "Century Gothic";
+	font-size: 15px;
+	font-variant: small-caps;
+}
+
+input#evenbtn {
+	border: 0px;
+	background: white;
+	font-family: Verdana;
+	font-size: 15px;
+}
+
+input#oddbtn {
+	border: 0px;
+	background: #384E82;
+	color: white;
+	font-family: Verdana;
+	font-size: 15px;
+}
+
+h1 {
+	color: white; 
+	font-size: 50px;
+	font-weight: bold; 
+	font-variant: small-caps;
+	font-family: Arial Black;
+	line-height: 45px; 
+}
+
+h2 {
+	color: white;
+	font-family: "Century Gothic"; 
+	font-weight: normal; 
+	font-size: 15px; 
+	background: #051C50; 
+	font-variant: small-caps; 
+	padding: 5px;
+}
+
+table#items {
+	padding: 15px;
+	font-family: Verdana;
+	align: middle;
+	font-size: 15px;
+}
+
+table#items tr:nth-child(even) {
+	background: #384E82;
+	color: white;
+}
+
+form {
+	display: inline;
+}
+
+</style>
+	<body>
+	<div id="wrapper">
+	<div id="left" class="column">&nbsp;</div>
+	<div id="middle" class="column">
+		<center>
+			<a href="computerstore.php">
+				<IMG SRC="304Matrix.jpg" alt="Make sure you put 304Matrix.jpg in the correct folder" width =100% height="360"/>
+			</a>
+			<div id="nav"><h2><a href="login.php">Login</a> | 
+							  <a href="account.php">My Account</a> |
+							  <a href="shoppingcart.php">Shopping Cart</a> |
+							  <?php
+								if ((!empty($_SESSION)) && ($_SESSION['admin'] = "true")) { ?>
+							  	  <a href="admin.php">Admin</a> | 
+								<?php } else { ?>
+								  <a href="adminlogin.php">Admin</a> |
+								<?php }	?>
+								  
+							<form action="logout.php"><input id="logoutbtn" type="submit" value="Log Out"></input></form>
+						  </h2>
+			</div>
+			<div id="statusbar">
+				<?php
+					try{
+						if(!$pdo = new PDO('mysql:host=localhost;dbname=computerstoredb',
+						'root',
+						'admin1')
+						){
+					$sad = "\r\n :( \r\n";
+					echo nl2br($sad);
+					exit;
+					}
+	
+	$yay = "\r\n Status: Connected to Database! \r\n";
+	echo $yay;
+	}
+	catch (PDOException $Exception){
+		$error = "\r\nCould not connect: " . $Exception->getMessage( );
+		echo nl2br($error);
+	}
+	
+	session_start();
+	
+	if (!empty($_SESSION["user"])) {
+		echo "<br />" . "Welcome " . $_SESSION["user"];
+	} else {
+		echo "<br />" . "Currently not logged in.";
+	}
+	
+/* 	$sql = "SELECT c_name FROM customer_account ORDER BY c_name";
+	echo "Customers: <br />";
+	foreach ($pdo->query($sql) as $row) {
+		echo nl2br($row['c_name']);
+		echo "<br />";
+	}
+	 */
+				?>
+			</div>
+			<div id="searchbar">
+				<form action="search.php" method="post">
+				<select name="itemType">
+					<option value="0">Select an item type</option>
+					<option value="1">Case</option>
+					<option value="2">Headset</option>
+					<option value="3">Monitor</option>
+					<option value="4">Motherboard</option>
+					<option value="5">Mouse</option>
+					<option value="6">Power Supply</option>
+					<option value="7">Processor</option>
+					<option value="8">RAM</option>
+					<option value="9">SSD</option>
+					<option value="10">Video Card</option>
+				</select>
+				<input type="text" name="search" class="search" value="" placeholder="Search for Product Name..."></input>				
+				<input type="submit" value="Search"/>
+				</form>
+				<form action="advancedsearch.php">
+					<button>Advanced Search</button>
+				</form> <br><br>
+			</div>
+			<div>
+			<?php
+			if (empty($_SESSION)) {
+				echo "You are not logged in!";
+			}
+			else {
+			?>
+				<form action="account.php" method="post" action="<?php echo $_SERVER["PHP_SELF"]?>">
+					<input id="delete" type="submit"  name="histbtn" value="Clear History">
+				</form>	
+			<?php }
+			
+			if (!empty($_POST['histbtn'])) {
+				$user = $_SESSION['user'];
+				$sql = "DELETE FROM purchasehistory_contains_purchase WHERE c_id = (SELECT c_id FROM customer_account WHERE c_name = '$user')";
+				$statement = $pdo->prepare($sql);
+				$statement->execute();
+				echo "Deleted your purchase history";
+			}
+			
+			?>
+			</div>
+		</center>
+		<center>
+
+	</div></center>
+	<div id="right" class="column">&nbsp;</div>
+
+</body>
 </html>
